945ec4af85d223b48c1000bb90bca6cfd682710c
OSHO <oshomani2006@gmail.com>
2026-09-20 07:54:16 +0530

    Fix mobile menu and holodeck contrast

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -144,6 +144,7 @@ export default function Header() {
           >
             <span></span>
             <span></span>
+            <span></span>
           </button>
         </div>
       </div>
